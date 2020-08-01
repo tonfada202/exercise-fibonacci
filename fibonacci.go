bc53e5e73bc05ef9aaa3065f9368450f3313443b
Autsadakorn Jarernket <autsadakorn.j@kbtg.tech>
2020-08-01 19:02:21 +0700

    ส่งการบ้านครับ

---
+++ fibonacci.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	var x, y int = 1, 0
+	return func() int {
+		y, x = (x + y), y
+		return x
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
