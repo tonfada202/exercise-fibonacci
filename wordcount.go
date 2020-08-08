a8c47b8fb0f492edb3e9d54ade57317c04a9a195
Autsadakorn Jarernket <autsadakorn.j@kbtg.tech>
2020-08-08 18:16:53 +0700

    ส่งการบ้าน day2 word count

---
+++ wordcount.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	strParts := strings.Split(s, " ")
+	m := make(map[string]int)
+	for _, key := range strParts {
+		d, ok := m[key]
+		if ok {
+			m[key] = d + 1
+		} else {
+			m[key] = 1
+		}
+	}
+	return m
+}
+
+func main() {
+	s := "If it looks like a duck, swims like a duck, and quacks like a duck, then it probably is a duck,"
+	w := WordCount(s)
+	fmt.Println("---- WordCount ----")
+	for index, value := range w {
+		fmt.Println(index, "=", value)
+	}
+	fmt.Printf("***** total = %d ***** \n ", len(w))
+}
