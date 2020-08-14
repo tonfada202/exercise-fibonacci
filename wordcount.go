708cadc4c6d24b5d143083e5bafa500d4aa985e0
Autsadakorn Jarernket <autsadakorn.j@kbtg.tech>
2020-08-14 17:12:26 +0700

    ส่งการบ้าน

--- wordcount.go
+++ wordcount.go
@@ -20,7 +20,7 @@ func WordCount(s string) map[string]int {
 }
 
 func main() {
-	s := "If it looks like a duck, swims like a duck, and quacks like a duck, then it probably is a duck,"
+	s := "If it looks like a duck, swims like a duck, and quacks like a duck, then it probably is a duck."
 	w := WordCount(s)
 	fmt.Println("---- WordCount ----")
 	for index, value := range w {
